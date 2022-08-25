b8a781201b4ac63ad043cc9dfd8c4542d70f3d28
itsvvill <w.lewis.eason@gmail.com>
2022-08-25 16:22:46 -0700

    added react router set up routing for home login and signup pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom';
+// styles
+import './App.css';
+//components
+import Home from './pages/home/Home';
+import Login from './pages/login/login';
+import Signup from './pages/signup/signup';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Route exact path="/home">
+        <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
