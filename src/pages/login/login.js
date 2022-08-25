a064a43ab953e9a52394f70578fbd772fe1f0c02
itsvvill <w.lewis.eason@gmail.com>
2022-08-25 16:13:19 -0700

    continued to build out pages and styles sheets

---
+++ src/pages/login/login.js
@@ -0,0 +1,6 @@
+//styles
+import styles from './Login.module.css';
+
+export default function Login() {
+  return <div>Login</div>;
+}
---
+++ src/pages/signup/signup.js
@@ -0,0 +1,6 @@
+//styles
+import styles from './Signup.module.css';
+
+export default function Signup() {
+  return <div>Sign Up</div>;
+}
