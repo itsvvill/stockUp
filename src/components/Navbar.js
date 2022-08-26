236712a53aacbc55864faa8f786d2db7a019900f
itsvvill <w.lewis.eason@gmail.com>
2022-08-26 10:16:28 -0700

    added navbar links to signup and login pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+
 // styles
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
-  return <div>Nav</div>;
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
