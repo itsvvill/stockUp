bc9994be5622564fb768882ce321f19c28b54b08
itsvvill <w.lewis.eason@gmail.com>
2022-08-27 10:39:54 -0700

    built out signup page

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,6 +1,45 @@
+import { useState } from 'react';
+
 //styles
 import styles from './Signup.module.css';
 
 export default function Signup() {
-  return <div>Sign Up</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="displayname"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Sign up</button>
+    </form>
+  );
 }
