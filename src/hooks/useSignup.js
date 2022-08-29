feb5711c0117b990381a7d33ac698c1b0a25a415
itsvvill <w.lewis.eason@gmail.com>
2022-08-29 14:05:48 -0700

    added try catch for signup

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -5,7 +5,18 @@ export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
 
-  const signup = (async = (email, password, displayName) => {});
+  const signup = (async = (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //signup user
+    } catch (err) {
+      console.log(err);
+      setError(err.message);
+      setIsPending(false);
+    }
+  });
 
   return { error, isPending, signup };
 };
