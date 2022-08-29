fb69c099dcf86bef6be47cd84e526d28865751a6
itsvvill <w.lewis.eason@gmail.com>
2022-08-29 10:16:18 -0700

    removed dead code from config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,7 +8,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementID: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 //init firebase
