2d330b47c0c6bfb01f82510be99345ab3468b39a
itsvvill <w.lewis.eason@gmail.com>
2022-08-29 10:53:33 -0700

    started building out signup function, added return

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -4,4 +4,8 @@ import { projectAuth } from '../firebase/config';
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+
+  const signup = (async = (email, password, displayName) => {});
+
+  return { error, isPending, signup };
 };
