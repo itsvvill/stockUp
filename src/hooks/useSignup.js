db728e0876006b1e79dc2de00b376b672598876d
itsvvill <w.lewis.eason@gmail.com>
2022-08-29 10:47:41 -0700

    started building out custom hook to signup new user

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+};
