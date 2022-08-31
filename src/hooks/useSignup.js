640821a7425dd72c6fc055800eea17ec9f47b6c4
itsvvill <w.lewis.eason@gmail.com>
2022-08-31 09:29:40 -0700

    reset isPending and setError for happy case

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -20,6 +20,9 @@ export const useSignup = () => {
 
       // add display name to user
       await res.user.updateProfile({displayName})
+      
+      setIsPending(false);
+      setError(null);
 
     } catch (err) {
       console.log(err);
