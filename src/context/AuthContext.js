cf59ba030a9fd717af0a4c3ff079ff9e6f63b968
itsvvill <w.lewis.eason@gmail.com>
2022-09-02 07:58:38 -0700

    continued to build out auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,14 @@
 import { createContext, useReducer } from 'react';
 
 export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
