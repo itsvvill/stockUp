dfd25dfe799cb0fb2ca3dddba594991183cc2499
itsvvill <w.lewis.eason@gmail.com>
2022-09-02 06:36:46 -0700

    fixed some bugs with useSignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -5,31 +5,33 @@ export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
 
-  const signup = (async = (email, password, displayName) => {
+  const signup = async (email, password, displayName) => {
     setError(null);
     setIsPending(true);
 
     try {
       //signup user
-      await projectAuth.createUserWithEmailAndPassword(email, password)
-      console.log(res.user)
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user);
 
       if (!res) {
-        throw new Error('Could not complete signup')
+        throw new Error('Could not complete signup');
       }
 
       // add display name to user
-      await res.user.updateProfile({displayName})
-      
+      await res.user.updateProfile({ displayName });
+
       setIsPending(false);
       setError(null);
-
     } catch (err) {
       console.log(err);
       setError(err.message);
       setIsPending(false);
     }
-  });
+  };
 
   return { error, isPending, signup };
 };
