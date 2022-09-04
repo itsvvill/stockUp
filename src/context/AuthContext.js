ce8d46982b267dff20140599e520dc9bcf0bccbd
itsvvill <w.lewis.eason@gmail.com>
2022-09-04 06:22:45 -0700

    added console log to authContext

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -15,6 +15,9 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  console.log('AuthContext state:', state);
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
