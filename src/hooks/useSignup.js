a69ef9161469dde1efe1306dc43f9cafff0fc29f
itsvvill <w.lewis.eason@gmail.com>
2022-09-04 06:18:11 -0700

    added authContext to useSignup to dispatch login action

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signUp = async (email, password, displayName) => {
     setError(null);
@@ -15,7 +17,6 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(res.user);
 
       if (!res) {
         throw new Error('Could not complete signup');
@@ -23,6 +24,9 @@ export const useSignup = () => {
 
       // add display name to user
       await res.user.updateProfile({ displayName });
+
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
 
       setIsPending(false);
       setError(null);
