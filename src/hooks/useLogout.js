03b129b0d5f1126c23b4e79c3b504d2735dca296
itsvvill <w.lewis.eason@gmail.com>
2022-09-05 06:28:01 -0700

    added error handling for logout function

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -6,4 +6,17 @@ const useLogout = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    //sign the user out
+    try {
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
 };
