7daef58897d249255d8314444b84194d8952ed60
itsvvill <w.lewis.eason@gmail.com>
2022-09-05 06:26:17 -0700

    added state to useLogout hook

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -2,4 +2,8 @@ import { useState } from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
-const useLogout = () => {};
+const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+};
