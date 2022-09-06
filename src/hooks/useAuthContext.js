5356cba7f4e35f6ededee1d432d25a588adc9944
itsvvill <w.lewis.eason@gmail.com>
2022-09-06 10:10:50 -0700

    fixed bug with AuthContext

--- src/hooks/useAuthContext.js
+++ src/hooks/useAuthContext.js
@@ -2,7 +2,7 @@ import { AuthContext } from '../context/AuthContext';
 import { useContext } from 'react';
 
 export const useAuthContext = () => {
-  const context = useContext();
+  const context = useContext(AuthContext);
 
   if (!context) {
     throw Error('useAuthContext must be inside an AuthContextProvider');
