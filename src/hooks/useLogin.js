d6ab1b0cb117516ffa40f7c8d21fa02dcea58d7a
itsvvill <w.lewis.eason@gmail.com>
2022-09-08 13:17:42 -0700

    started building out useLogin hook, added imports

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { projectAuth } from '../firebase/config';
+import { AuthContext } from '../context/AuthContext';
+
+export const useLogin = () => {};
