eed852be1e0a0f93a5e393d407e638481375be9b
itsvvill <w.lewis.eason@gmail.com>
2022-09-09 11:08:00 -0700

    continued to build out useLogin hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { AuthContext } from '../context/AuthContext';
+import { useAuthContext } from './useAuthContext';
 
 export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async () => {
+    setError(null);
+    setIsPending(true);
+  };
 };
