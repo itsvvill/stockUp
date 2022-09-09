1f8aef2a990ddfba538cbfe7aaa62daade91481f
itsvvill <w.lewis.eason@gmail.com>
2022-09-09 10:17:03 -0700

    added state to useLogin hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,4 +2,8 @@ import { useState, useEffect } from 'react';
 import { projectAuth } from '../firebase/config';
 import { AuthContext } from '../context/AuthContext';
 
-export const useLogin = () => {};
+export const useLogin = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+};
