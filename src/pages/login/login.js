ea028f458b431297610d248f1e20dcf60cc8779a
itsvvill <w.lewis.eason@gmail.com>
2022-09-10 11:08:38 -0700

    imported and added useLogin hook to Login component

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useLogin } from '../../hooks/useLogin';
 
 // styles
 import styles from './Login.module.css';
@@ -6,10 +7,11 @@ import styles from './Login.module.css';
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
