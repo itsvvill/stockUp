827e171c9b3a9cfd3d957af38ae9889e6937a6a7
itsvvill <w.lewis.eason@gmail.com>
2022-09-10 10:27:34 -0700

    added useEffect, try catch to useLogin hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -12,5 +12,29 @@ export const useLogin = () => {
   const login = async () => {
     setError(null);
     setIsPending(true);
+
+    //log the user in
+    try {
+      await // dispatch login action
+      dispatch({ type: 'LOGIN' });
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
   };
+  useEffect =
+    (() => {
+      return () => setIsCancelled(true);
+    },
+    []);
+  return { login, error, isPending };
 };
