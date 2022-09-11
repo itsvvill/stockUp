d8cbf25f834a1400ddd82108e9974bfda21bbab0
itsvvill <w.lewis.eason@gmail.com>
2022-09-11 08:03:26 -0700

    added conditional rendering for navbar links based on user being logged in or not

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,20 +7,29 @@ import styles from './Navbar.module.css';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>myMoney</li>
-        <li>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Logout
-          </button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          </li>
+        )}
       </ul>
     </nav>
   );
