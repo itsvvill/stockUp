3d703283776e446017e554fc079e6a2c1250e414
itsvvill <w.lewis.eason@gmail.com>
2022-09-13 11:07:17 -0700

    started building out the home component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,5 +2,10 @@
 import styles from './Home.module.css';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction List</div>
+      <div className={styles.sidebar}>Transaction Form</div>
+    </div>
+  );
 }
