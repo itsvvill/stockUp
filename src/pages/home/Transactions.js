8259b4013f07e3bac3dff8a36695e657bf21055d
itsvvill <w.lewis.eason@gmail.com>
2022-09-14 09:47:56 -0700

    started building out Transactions component

---
+++ src/pages/home/Transactions.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export default function TransactionForm() {
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState();
+  return <div></div>;
+}
