84223d41f8e043f3a672ebe45d7a0cd63b285c91
itsvvill <w.lewis.eason@gmail.com>
2022-09-14 10:03:07 -0700

    added form and inputs for transaction name and amount, submit function

--- src/pages/home/Transactions.js
+++ src/pages/home/Transactions.js
@@ -2,6 +2,35 @@ import { useState } from 'react';
 
 export default function TransactionForm() {
   const [name, setName] = useState('');
-  const [amount, setAmount] = useState();
-  return <div></div>;
+  const [amount, setAmount] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <h3>Add a Transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Transaction Name</span>
+          <input
+            type="text"
+            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Transaction Amount</span>
+          <input
+            type="number"
+            required
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+          />
+        </label>
+        <button>Add Transaction</button>
+      </form>
+    </>
+  );
 }
