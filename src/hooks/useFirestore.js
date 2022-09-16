37c9e9ab51f525c9e27128cde69ba945637f8663
itsvvill <w.lewis.eason@gmail.com>
2022-09-16 12:28:07 -0700

    added try catch and built out more of firestoreReducer

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -11,7 +11,26 @@ let initialState = {
 const firestoreReducer = (state, action) => {
   switch (action.type) {
     case 'IS_PENDING':
-      return { ...state, isPending: true };
+      return {
+        isPending: true,
+        document: null,
+        success: false,
+        error: null,
+      };
+    case 'ADDED_DOCUMENT':
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case 'ERROR':
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -24,9 +43,22 @@ export const useFirestore = (collection) => {
   // collection ref
   const ref = projectFirestore.collection(collection);
 
+  // only dispatch if not cancelled
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   // add a document
-  const addDocument = (doc) => {
-    dispatch({ type: 'IS_PENDING' });
+  const addDocument = async (doc) => {
+    dispatchIfNotCancelled({ type: 'ADDED_DOCUMENT', payload: addedDocument });
+
+    try {
+      const addedDocument = await ref.add(doc);
+    } catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message });
+    }
   };
 
   // delete a document
