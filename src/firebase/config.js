c8520ec3097162d163fdf34b2f883b83c59653b0
itsvvill <w.lewis.eason@gmail.com>
2022-09-17 09:25:12 -0700

    added timestamp variable to firebase config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,4 +18,7 @@ firebase.initializeApp(firebaseConfig);
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
 
-export { projectFirestore, projectAuth };
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
