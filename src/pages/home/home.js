cc1352eda04b10c2858feaae3d8420ece6c8b2ca
itsvvill <w.lewis.eason@gmail.com>
2022-09-18 08:23:24 -0700

    imported useAuthContext to Home component to pass user id to TransactionForm

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,16 @@
+import { useAuthContext } from '../../hooks/useAuthContext';
 //styles
 import styles from './Home.module.css';
 // components and pages
 import TransactionForm from './TransactionForm';
 
 export default function Home() {
+  const { user } = useAuthContext;
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction List</div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
