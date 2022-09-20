d1fcdce8c2eb57d92ccc58543ec997f211dff8b6
itsvvill <w.lewis.eason@gmail.com>
2022-09-20 11:05:34 -0700

    added state, useEffect function

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,4 +1,11 @@
 import { useEffect, useState } from 'react';
 import { projectFirestore } from '../firebase/config';
 
-export const useCollection = (collection) => {};
+export const useCollection = (collection) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection);
+  }, [collection]);
+};
