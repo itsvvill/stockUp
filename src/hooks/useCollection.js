aa8463c279495f706e811edda2094ac4a9040478
itsvvill <w.lewis.eason@gmail.com>
2022-09-21 09:05:28 -0700

    added error handling for useCollection hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -8,16 +8,22 @@ export const useCollection = (collection) => {
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
 
-    const unsubscribe = ref.onSnapshot((snapshot) => {
-      let results = [];
-      snapshot.docs.forEach((doc) => {
-        results.push({ ...doc.data(), id: doc.id });
-      });
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let results = [];
+        snapshot.docs.forEach((doc) => {
+          results.push({ ...doc.data(), id: doc.id });
+        });
 
-      //update state
-      setDocuments(results);
-      setError(null);
-    });
+        //update state
+        setDocuments(results);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError('could not fetch the data');
+      }
+    );
     // unsubscribe on unmount
     return () => unsubscribe();
   }, [collection]);
