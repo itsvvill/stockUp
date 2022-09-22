5638a1a43cbd431a86c524423077ee116a0e3644
itsvvill <w.lewis.eason@gmail.com>
2022-09-22 11:21:49 -0700

    added error and documents to home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useCollection } from '../../hooks/useCollection';
+
 //styles
 import styles from './Home.module.css';
 // components and pages
@@ -7,9 +8,12 @@ import TransactionForm from './TransactionForm';
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection('transactions');
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction List</div>
+      {error && <p>{error}</p>}
+      {documents && <TransactionList transactions={documents} />}
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
