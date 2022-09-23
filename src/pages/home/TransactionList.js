426269274c4fc58e5e775a4ad33b306eae849790
itsvvill <w.lewis.eason@gmail.com>
2022-09-23 12:34:58 -0700

    finished building out TransactionList component

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -5,7 +5,10 @@ export default function TransactionList({ transactions }) {
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
-        <li key={transaction.id}></li>
+        <li key={transaction.id}>
+          <p className={styles.name}>{transaction.name}</p>
+          <p className={styles.amount}>${transaction.amount}</p>
+        </li>
       ))}
     </ul>
   );
