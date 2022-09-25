241d2298d72f343c3bad24d0c927cb5e365f8a23
itsvvill <w.lewis.eason@gmail.com>
2022-09-25 10:01:02 -0700

    changed collection to include array for query

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -9,7 +9,11 @@ import TransactionList from './TransactionList';
 
 export default function Home() {
   const { user } = useAuthContext();
-  const { documents, error } = useCollection('transactions');
+  const { documents, error } = useCollection('transactions', [
+    'uid',
+    '==',
+    user.id,
+  ]);
   return (
     <div className={styles.container}>
       <div className={styles.content}>
