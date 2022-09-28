3f1f91f7a32474f09f8cb2cc70fc5d2f58ae65a1
itsvvill <w.lewis.eason@gmail.com>
2022-09-28 08:49:10 -0700

    added useFirestore import to TransactionList component

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -1,7 +1,11 @@
+import { useFirestore } from '../../hooks/useFirestore';
+
 // styles
 import styles from './Home.module.css';
 
 export default function TransactionList({ transactions }) {
+  const { deleteDocument } = useFirestore(transactions);
+
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
