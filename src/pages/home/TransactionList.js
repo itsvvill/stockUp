0af2cc9e6f99e36f02057c8a0326afd5f51dc889
itsvvill <w.lewis.eason@gmail.com>
2022-09-29 09:11:49 -0700

    added button to delete a transaction from TransactionList component

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -12,6 +12,7 @@ export default function TransactionList({ transactions }) {
         <li key={transaction.id}>
           <p className={styles.name}>{transaction.name}</p>
           <p className={styles.amount}>${transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
     </ul>
