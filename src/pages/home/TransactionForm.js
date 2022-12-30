6af98c64023094d01c5f52761c9f523087f31091
itsvvill <w.lewis.eason@gmail.com>
2022-12-30 09:03:17 -0800

    updated TransactionForm component to include categories

--- src/pages/home/TransactionForm.js
+++ src/pages/home/TransactionForm.js
@@ -5,6 +5,7 @@ export default function TransactionForm({ uid }) {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
   const [date, setDate] = useState('');
+  const [category, setCategory] = useState('');
   const { addDocument, response } = useFirestore('transactions');
 
   const handleSubmit = (e) => {
@@ -14,6 +15,7 @@ export default function TransactionForm({ uid }) {
       name,
       amount,
       date,
+      category,
     });
   };
 
@@ -22,6 +24,8 @@ export default function TransactionForm({ uid }) {
     if (response.success) {
       setName('');
       setAmount('');
+      setDate('');
+      setCategory('');
     }
   }, [response.success]);
 
@@ -55,6 +59,35 @@ export default function TransactionForm({ uid }) {
             onChange={(e) => setDate(e.target.value)}
             value={date}
           />
+        </label>
+        <label>
+          <span>Category:</span>
+          <select
+            required
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
+            <option value="">-- Choose an option --</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+            <option value="Rent/Mortgage">Rent/Mortgage</option>
+            <option value="Electricity">Electricity</option>
+            <option value="Water">Water</option>
+            <option value="Internet">Internet</option>
+            <option value="Cellphone">Cellphone</option>
+            <option value="Groceries">Groceries</option>
+            <option value="Eating Out">Eating Out</option>
+            <option value="Transportation">Transportation</option>
+            <option value="Home Maintenance">Home Maintenance</option>
+            <option value="Auto Maintenance">Auto Maintenance</option>
+            <option value="Gifts">Gifts</option>
+            <option value="Vacation">Vacation</option>
+            <option value="Education">Education</option>
+            <option value="Home Improvement">Home Improvement</option>
+            <option value="Hobbies">Hobbies</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Health & Wellness">Health & Wellness</option>
+            <option value="Other">Other</option>
+          </select>
         </label>
         <button>Add Transaction</button>
       </form>
