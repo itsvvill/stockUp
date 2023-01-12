748ceaaabb723bc977e4e2dc8ca6f8f3a6f211a4
itsvvill <w.lewis.eason@gmail.com>
2023-01-12 09:45:06 -0800

    removed auto import

--- src/pages/stocks/Stocks.js
+++ src/pages/stocks/Stocks.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react/cjs/react.production.min';
 
 // styles
 import styles from './Stocks.module.css';
