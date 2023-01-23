dae4fbb08ba21bc8aaf4651135cc2ba399176385
itsvvill <w.lewis.eason@gmail.com>
2023-01-23 09:57:37 -0800

    added reducer for editing or updating a document

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -19,6 +19,13 @@ const firestoreReducer = (state, action) => {
         success: true,
         error: null,
       };
+    case 'UPDATED_DOCUMENT':
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
     case 'DELETED_DOCUMENT':
       return { isPending: false, document: null, success: true, error: null };
     case 'ERROR':
@@ -74,10 +81,26 @@ export const useFirestore = (collection) => {
       dispatchIfNotCancelled({ type: 'ERROR', payload: 'could not delete' });
     }
   };
+  //update a document
+  const updateDocument = async (id, updates) => {
+    dispatch({ type: 'IS_PENDING' });
+
+    try {
+      const updatedDocument = await ref.doc(id).update(updates);
+      dispatchIfNotCancelled({
+        type: 'UPDATED_DOCUMENT',
+        payload: updateDocument,
+      });
+      return updatedDocument;
+    } catch (err) {
+      dispatchIfNotCancelled({ type: 'ERROR', payload: err.message });
+      return null;
+    }
+  };
 
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
 
-  return { addDocument, deleteDocument, response };
+  return { addDocument, deleteDocument, updateDocument, response };
 };
