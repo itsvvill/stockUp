917fb61f35d67b2032397b03f8418c5381e242de
itsvvill <w.lewis.eason@gmail.com>
2023-02-11 11:17:47 -0800

    added functions to find average, min, max of transactions

--- src/pages/home/TransactionInfo.js
+++ src/pages/home/TransactionInfo.js
@@ -1,3 +1,33 @@
 export default function TransactionInfo({ transactions }) {
-  return <div>Info</div>;
+  let numberOfTransactions = transactions.length;
+  let total = transactions.reduce((acc, n) => acc + parseInt(n.amount), 0);
+  let average = Math.round(total / numberOfTransactions);
+
+  function getMinOrMax(array, modifier) {
+    let minOrMax;
+    if (modifier === 'max') {
+      minOrMax = -Infinity;
+      array.forEach(
+        (object) => (minOrMax = Math.max(minOrMax, parseInt(object.amount)))
+      );
+    } else {
+      minOrMax = Infinity;
+      array.forEach(
+        (object) => (minOrMax = Math.min(minOrMax, parseInt(object.amount)))
+      );
+    }
+    return minOrMax;
+  }
+  return (
+    <>
+      {transactions.length >= 1 && (
+        <div>
+          Smallest Transaction: ${getMinOrMax(transactions, 'min')} Largest
+          Transaction: ${getMinOrMax(transactions, 'max')}
+          Total: {total}
+          Average: {average}
+        </div>
+      )}
+    </>
+  );
 }
