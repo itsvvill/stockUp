55aaf7d5448339d386f6f4fb1fca436d82fbc467
itsvvill <w.lewis.eason@gmail.com>
2023-02-20 15:27:24 -0800

    built out careers page and added to guide page

--- src/pages/careers/Careers.js
+++ src/pages/careers/Careers.js
@@ -1,74 +1,80 @@
+//styles and icons
 import styles from './Careers.module.css';
 import logo from '../../components/logo.png';
+import { UilUserCircle } from '@iconscout/react-unicons';
+
 export default function Careers() {
+  //mock job info
   const opportunities = [
     {
       date: new Date().toDateString(),
       department: 'Engineering',
       title: 'Front-End Engineer',
-      description: 'Join the StockUp team as a Front-End Engineer',
+      description: 'Delight our users with your designs...',
       available: false,
       recruiter: 'Steve Stylesheets',
-      recruiterImage: '',
       id: '1208734ASDFaf93121',
     },
     {
       date: new Date().toDateString(),
       department: 'Research',
       title: 'Data Scientist',
-      description: 'Join the StockUp team as a Data Scientest',
+      description: 'Empower our services with data...',
       available: false,
       recruiter: 'Wonda Cuditbee',
-      recruiterImage: '',
       id: '12125FafSFCA121',
     },
     {
       date: new Date().toDateString(),
       department: 'Finance',
       title: 'Staff Accountant',
-      description: 'Join the StockUp team as a Staff Accountant',
+      description: 'Everyone needs some checks and balances...',
       available: false,
       recruiter: 'Penny Pinchers',
-      recruiterImage: '',
       id: '208208FASHW392',
     },
     {
       date: new Date().toDateString(),
       department: 'Engineering',
       title: 'FullStack Engineer',
-      description: 'Join the StockUp team as a Full Stack Engineer',
+      description: 'Create the next great finance product at scale...',
       available: false,
       recruiter: 'Al Gorithm',
-      recruiterImage: '',
       id: '08192312VBASD921',
     },
   ];
+
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
-        <h1>
+        <h1 className={styles.h1}>
           Careers at{' '}
           <img src={logo} className={styles['logo']} alt="StockUp Logo" />
           tockUp.
         </h1>
-        <p>See our job opportunities below.</p>
+        <p>See our career opportunities below.</p>
       </div>
       <div className={styles['jobs-container']}>
         {opportunities.map((job) => (
           <div className={styles['job-card']} key={job.id}>
-            <div>
-              <p>{job.date}</p>
-              <p>{job.department}</p>
+            <div className={styles['top-line']}>
+              <p className={styles.date}>{job.date}</p>
+              <p className={styles.department}>{job.department}</p>
             </div>
-            <p>{job.title}</p>
-            <p>{job.description}</p>
+            <p className={styles.title}>{job.title}</p>
+            <p className={styles.description}>{job.description}</p>
             <div>
               <div>
-                <p>Position {job.available ? 'Available' : 'Unavailable'}</p>
+                <p className={styles.status}>
+                  Position Status:{' '}
+                  <span className={styles['status-span']}>
+                    {job.available ? 'Available' : 'Unavailable'}
+                  </span>
+                </p>
               </div>
-              <div>
-                <image src={job.recruiterImage} alt="recruiter image" />
-                <p>{job.recruiter}</p>
+              <div className={styles['recruiter-container']}>
+                Recruiting Contact: <UilUserCircle size="20" />
+                <p className={styles.recruiter}>{job.recruiter}</p>
               </div>
             </div>
           </div>
--- src/pages/guide/Guide.js
+++ src/pages/guide/Guide.js
@@ -1,3 +1,18 @@
+//styles and icons
+import styles from './Guide.module.css';
+import logo from '../../components/logo.png';
+
 export default function Guide() {
-  return <div>Guide</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>
+          The Official{' '}
+          <img src={logo} className={styles['logo']} alt="StockUp Logo" />
+          tockUp Guide.
+        </h1>
+        <p>Follow the guide below to get started with StockUp today!</p>
+      </div>
+    </div>
+  );
 }
