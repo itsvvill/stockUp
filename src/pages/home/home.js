a0965aee100a1127e2274a7e554228cd09d69909
itsvvill <w.lewis.eason@gmail.com>
2023-02-20 17:17:03 -0800

    built out Home page component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,33 @@
+import { Link } from 'react-router-dom';
+
 import styles from './Home.module.css';
 import logo from '../../components/logo.png';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>
-        <h1>
+      <div className={styles.intro}>
+        <h1 className={styles.header}>
           Welcome to{' '}
-          <img src={logo} className={styles['logo']} alt="StockUp Logo" />
-          tockUp!
+          <span>
+            <img src={logo} className={styles['logo']} alt="StockUp Logo" />
+            tockUp!
+          </span>
         </h1>
-        <p>A financial website for smarter decision making.</p>
+        <h2>A financial website for smarter decision making.</h2>
+        <ul className={styles.blurb}>
+          <li>Search for real-time stock prices and information.</li>
+          <li>Create a custom watchlist of the stocks you care about.</li>
+          <li>Stay on top of your budget with our transaction tracker.</li>
+        </ul>
+        <div className={styles['button-container']}>
+          <Link to="/guide" className={styles.button}>
+            Learn More
+          </Link>
+          <Link to="/login" className={styles.button}>
+            Get Started
+          </Link>
+        </div>
       </div>
     </div>
   );
