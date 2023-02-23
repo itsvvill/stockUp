71cee32bc57b79d922c7dd34c9a4259a652f3de6
itsvvill <w.lewis.eason@gmail.com>
2023-02-22 18:12:38 -0800

    built out Guide page

--- src/pages/guide/Guide.js
+++ src/pages/guide/Guide.js
@@ -13,16 +13,42 @@ export default function Guide() {
           <img src={logo} className={styles['logo']} alt="StockUp Logo" />
           tockUp Guide.
         </h1>
-        <p>Follow the guide below to get started with StockUp today!</p>
+        <p className={styles.instructions}>
+          Follow the guide below to get started with StockUp today!
+        </p>
       </div>
-      <div>
-        <h2>Step 1: Log Into StockUp</h2>
-        <p>
-          Log in to your StockUp account. If you don't have an account, you can
-          create one by clicking the button below.
+      <div className={styles.card}>
+        <h2 className={styles['card-heading']}>Step 1: Log Into StockUp</h2>
+        <p className={styles.p}>
+          Login to StockUp, try it out using a guest account, or sign up for a
+          new account by clicking the button below.
         </p>
         <Link to="/login" className={styles.button}>
           Login
+        </Link>
+      </div>
+      <div className={styles.card}>
+        <h2 className={styles['card-heading']}>
+          Step 2: Check on your favorite stocks
+        </h2>
+        <p className={styles.p}>
+          Search for real-time prices of a single stock or create a watchlist to
+          track a group of stocks.
+        </p>
+        <Link to="/stocks" className={styles.button}>
+          Stocks
+        </Link>
+      </div>
+      <div className={styles.card}>
+        <h2 className={styles['card-heading']}>
+          Step 3: Stay on top of your finances
+        </h2>
+        <p className={styles.p}>
+          Use our transaction list feature to easily categorize and sort your
+          finances.
+        </p>
+        <Link to="/transactions" className={styles.button}>
+          Transactions
         </Link>
       </div>
     </div>
