3344edc8081648ebc570bcee48565258b425c46e
itsvvill <w.lewis.eason@gmail.com>
2023-02-27 11:02:38 -0800

    added more sections to TOS

--- src/pages/privacy/TermsOfService.js
+++ src/pages/privacy/TermsOfService.js
@@ -111,7 +111,81 @@ export default function TermsOfService() {
         a username you select if we determine, in our sole discretion, that such
         username is inappropriate, obscene, or otherwise objectionable.
       </p>
-      <h2>5. USER DATA</h2>
+      <h2>5. PROHIBITED ACTIVITIES</h2>
+      <p>
+        You may not access or use the Site for any purpose other than that for
+        which we make the Site available. The Site may not be used in connection
+        with any commercial endeavors except those that are specifically
+        endorsed or approved by us.
+      </p>
+      <p>As a user of the Site, you agree not to:</p>
+      <ul>
+        <li>
+          Systematically retrieve data or other content from the Site to create
+          or compete, directly or indirectly, a collection, compilation,
+          database, or directory without written permission from us.
+        </li>
+        <li>
+          Trick, defraud, or mislead us and other users, especially in any
+          attemp to learn sensitive information such as user passwords.
+        </li>
+        <li>
+          Circumvent, disable, or otherwise interefere with secruity-related
+          features of the Site, including features that prevent or restrict the
+          use of copying of any Content or enforce limitations on the use of the
+          Site and/or the Content contained therein.
+        </li>
+        <li>
+          Disparage, tarnish, or otherwise harm, in our opinion, us and/or the
+          Site.
+        </li>
+        <li>
+          Use any information obtained from the Site in order to harass, abuse,
+          or harm another person.
+        </li>
+        <li>
+          Make improper use of our support services or submit false reports of
+          abuse or misconduct.
+        </li>
+        <li>
+          Use the Site in a manner inconsistent with any applicable laws or
+          regulations.
+        </li>
+        <li>Engage in unauthorized framing of or linking to the Site.</li>
+        <li>
+          Upload or transmit (or attempt to upload or transmit) viruses, Trojan
+          horses, or other material, including excessive use of capital letters
+          and spamming (continuous posting of repetitive text), that interferes
+          with any party's uninterrupted use and enjoyment of the Site or
+          modifies, impairs, disrupts, alters, or interferes with the use,
+          features, functions, operation, or maintenance of the Site.
+        </li>
+        <li>
+          Engage in any automated use of the system, such as using scripts to
+          send comments or messages, or using any data mining, robots, or
+          similar data gathering and extraction tools.
+        </li>
+        <li>
+          Delete the copyright or other property rights notice from any Content.
+        </li>
+        <li>
+          Attempt to impersonate another user or person or use the username of
+          another user.
+        </li>
+        <li>
+          Upload or transmit (or attempt to upload or transmit) any material
+          that acts as a passive or active information collection or
+          transmission mechanism, including without limitation, clear graphics
+          interchange formats ("gifs"), 1x1 pixels, web bugs, cookies, or other
+          similar devices (sometimes referred to as "spyware" or "passive
+          collective mechanisms" or "pcms").
+        </li>
+        <li>
+          Interfere with, disrupt, or create an undue burden on the Site or the
+          networks or services connected to the Site.
+        </li>
+      </ul>
+      <h2>6. USER DATA</h2>
       <p>
         We will maintain certain data that you transmit to the Site for the
         purpose of managing the performance of the Site, as well as data
@@ -122,7 +196,25 @@ export default function TermsOfService() {
         corruption of any such data, and you hearby waive any right of action
         against us arising from any such loss or corruption of such data.
       </p>
-      <h2>6. CONTACT US</h2>
+      <h2>7. PRIVACY POLICY</h2>
+      <p>
+        We care about privacy and security. Please review our Privacy Policy:{' '}
+        <a
+          href="https://stockupfinance.app/privacy"
+          alt="Link to StockUp Privacy Policy"
+        >
+          https://stockupfinance.app/privacy
+        </a>
+        . By using the Site, you agree to be bound by our Privacy Policy, which
+        is incorporated into these Terms of Use. Please be advised the site is
+        hosted in the United States. If you access the Site from any other
+        region of the world with laws or other requirements governing personal
+        data collection, use, or disclosure that differ from applicable laws in
+        the United States, then through your continued use of the Site, you are
+        transferring your data to the United Staates, and you agree to have your
+        data transferred and processed in the United States.
+      </p>
+      <h2>8. CONTACT US</h2>
       <p>
         In order to resolve a complaint regarding the Site or to recieve further
         information regarding use of the Site, please contact us at:
