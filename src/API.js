c5c9ea99c0ae42b4096a34992c27e1a6b0be4649
itsvvill <w.lewis.eason@gmail.com>
2023-03-03 08:47:50 -0800

    removed Alphavantage api info from Config and API files

--- src/API.js
+++ src/API.js
@@ -1,21 +1,12 @@
-import {
-  ALPHA_VANTAGE_API_KEY,
-  ALPHA_VANTAGE_BASE_URL,
-  FINN_HUB_API_KEY,
-  FINN_HUB_BASE_URL,
-  FINN_HUB_URL,
-  ALPHA_VANTAGE_SEARCH_URL,
-  ALPHA_VANTAGE_OVERVIEW_URL,
-  ALPHA_VANTAGE_QUOTE_URL,
-} from './config';
+import { FINN_HUB_API_KEY, FINN_HUB_BASE_URL, FINN_HUB_URL } from './config';
 
 const apiSettings = {
-  fetchSearch: async (searchQuery) => {
-    const endpoint = `${searchQuery}&apikey=${ALPHAVANTAGEAPI}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    return data;
-  },
+  // fetchSearch: async (searchQuery) => {
+  //   const endpoint =
+  //   let response = await fetch(url);
+  //   let data = await response.json();
+  //   return data;
+  // },
   fetchQuote: async (stockSymbol) => {
     const endpoint = `${stockSymbol}&token=${FINN_HUB_API_KEY}`;
     let response = await fetch(url);
