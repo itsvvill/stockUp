1094f355169506bba0375c480c455a94189118de
itsvvill <w.lewis.eason@gmail.com>
2023-03-06 14:38:02 -0800

    added check to make sure only authenticated users can delete accounts

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -9,53 +9,94 @@ export default function User() {
 
   const handleDelete = (e) => {
     e.preventDefault();
-    if (!user.isAnonymous) {
-      deleteUser();
-      if (error) console.log(error);
-    }
+    deleteUser();
+    if (error) console.log(error);
   };
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <h1 className={styles.intro}>
-          Hi, {user.displayName ? user.displayName : 'Guest'} (
-          {user.email ? user.email : 'guest@guestdemo.com'})
-        </h1>
-        <p className={styles.instructions}>
-          Adjust your preferred settings here.
-        </p>
-        <h2 className={styles.section}>Change Name</h2>
-        <input
-          className={styles.input}
-          type="text"
-          required
-          placeholder={user.displayName ? user.displayName : 'Guest'}
-        />
-        <button className={styles.button} disabled>
-          Update Name
-        </button>
-        <h2 className={styles.section}>Add Profile Image</h2>
-        <button className={styles.button} disabled>
-          Add New Image
-        </button>
+    <>
+      {!user.isAnonymous && (
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <h1 className={styles.intro}>
+              Hi, {user.displayName ? user.displayName : 'Guest'} (
+              {user.email ? user.email : 'guest@guestdemo.com'})
+            </h1>
+            <p className={styles.instructions}>
+              Adjust your preferred settings here.
+            </p>
+            <h2 className={styles.section}>Change Name</h2>
+            <input
+              className={styles.input}
+              type="text"
+              required
+              placeholder={user.displayName ? user.displayName : 'Guest'}
+            />
+            <button className={styles.button} disabled>
+              Update Name
+            </button>
+            <h2 className={styles.section}>Add Profile Image</h2>
+            <button className={styles.button} disabled>
+              Add New Image
+            </button>
 
-        <h2 className={styles.section}>Account Management</h2>
-        {!isPending && (
-          <button className={styles.delete} onClick={(e) => handleDelete(e)}>
-            Delete Account
-          </button>
-        )}
-        {isPending && (
-          <button
-            className={styles.delete}
-            disabled
-            onClick={(e) => handleDelete(e)}
-          >
-            Deleting...
-          </button>
-        )}
-        {error && <p>Sorry, something went wrong...</p>}
-      </div>
-    </div>
+            <h2 className={styles.section}>Account Management</h2>
+            {!isPending && (
+              <button
+                className={styles.delete}
+                onClick={(e) => handleDelete(e)}
+              >
+                Delete Account
+              </button>
+            )}
+            {isPending && (
+              <button
+                className={styles.delete}
+                disabled
+                onClick={(e) => handleDelete(e)}
+              >
+                Deleting...
+              </button>
+            )}
+            {error && <p>Sorry, something went wrong...</p>}
+          </div>
+        </div>
+      )}
+
+      {user.isAnonymous && (
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <h1 className={styles.intro}>
+              Hi, {user.displayName ? user.displayName : 'Guest'} (
+              {user.email ? user.email : 'guest@guestdemo.com'})
+            </h1>
+            <p className={styles.instructions}>
+              Sorry, these features are reserved for users.
+            </p>
+            <h2 className={styles.section}>Change Name</h2>
+            <input
+              className={styles.input}
+              type="text"
+              required
+              placeholder={user.displayName ? user.displayName : 'Guest'}
+            />
+            <button className={styles.button} disabled>
+              Update Name
+            </button>
+            <h2 className={styles.section}>Add Profile Image</h2>
+            <button className={styles.button} disabled>
+              Add New Image
+            </button>
+            <h2 className={styles.section}>Account Management</h2>
+            <button
+              className={styles.delete}
+              disabled
+              onClick={(e) => handleDelete(e)}
+            >
+              Delete Account
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
