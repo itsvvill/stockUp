22052ab9d623c56cac837980030b53e81c2a8cd4
itsvvill <w.lewis.eason@gmail.com>
2023-03-11 11:11:54 -0800

    updated TOS

--- src/pages/privacy/TermsOfService.js
+++ src/pages/privacy/TermsOfService.js
@@ -359,7 +359,55 @@ export default function TermsOfService() {
         any reason, without notice. We have no obligataion to monitor your
         Contributions.
       </p>
-      <h2>9. PRIVACY POLICY</h2>
+      <h2>9. SOCIAL MEDIA</h2>
+      <p>
+        As a part of the functionality of the Site, you may link your account
+        with online accounts you have with third-party service providers (each
+        such account, a "Third-Party Account") by either: (1) providing your
+        Third-Party Account login information through the Site; or (2) allowing
+        us to access your Third-Party Account as is permitted under the
+        applicable terms and conditions that govern your use of each Third-Party
+        Account. You represent and warrant that you are entitled to disclose
+        your Third-Party Account login information to us and/or grant us access
+        to your Third-Party Account, without breach by you of nay of the terms
+        and conditions that govern your use of the applicable Third-Party
+        Account, and without obligating us to pay any fees or making us subject
+        to any usage limitations imposed by the third-party service provider of
+        the Third-Party Account. By granting us access to any Third-Party
+        Accounts, you understand that (1) we may access, make available, and
+        store (if applicable) any content that you have provided to and stored
+        in your Third-Party Account (the "Social Network Content") so that it is
+        available on and through the Site via your account, including without
+        limitation any friend lists and (2) we may submit to and receive from
+        your Third-Party Account additional infromation to the extent you are
+        notified when you link your account with the Third-Party Account.
+        Depeding on the Third-Party Accounts you choose and subject to the
+        privacy settings that you have set in such Third-Party Accounts,
+        personally identifiable information that you post to your Third-Party
+        Accounts may be available on and through your account on the Site.
+        Please note that if a Third-Party Account or associated service becomes
+        unavailable or our access to such Third Party Account is terminatede by
+        the third-party service provider, then Social Network Content may no
+        longer be available on or through the Site. You will have the ability to
+        disable the connection between your account on the Site and your
+        Third-Party Accounts at any time. PLEASE NOTE THAT YOUR RELATIONSHIP
+        WITH THE THIRD-PARTY SERVICE PROVIDERS ASSOCIATED WITH YOUR THIRD-PARTY
+        ACCOUNTS IS GOVERNED SOLELY BY YOUR AGREEMENT(S) WITH SUCH THIRD-PARTY
+        SERICE PROVIDERS. We make no effort to review any Social Network Content
+        for any purpose, including but not limited to, for accuracy, legality,
+        or non-infringement, and we are not responsible for any Social Network
+        Content. You acknowledge and agree that we may access your email address
+        book associated with a Third-Party Account and your contacts list stored
+        on your mobile device or table computer solely for purposes of
+        identifying and informing you of those contacts who have also registered
+        to use the Site. You can deactivate the connection between the Site and
+        your Third-Party Account by contacting us using the contact information
+        below or through your account settings (if applicable). We will attemp
+        to delete any information stored on our servers that was obtained
+        through such Third-Party Account, except the username and profile
+        picture that become associated with your account.
+      </p>
+      <h2>10. PRIVACY POLICY</h2>
       <p>
         We care about privacy and security. Please review our Privacy Policy:{' '}
         <a
@@ -377,7 +425,7 @@ export default function TermsOfService() {
         transferring your data to the United Staates, and you agree to have your
         data transferred and processed in the United States.
       </p>
-      <h2>10. MISCELLANEOUS</h2>
+      <h2>11. MISCELLANEOUS</h2>
       <p>
         These Terms of Use and policies or operating rules posted by us on the
         Site or in respect to the Site constitute the entire agreement and
@@ -399,7 +447,7 @@ export default function TermsOfService() {
         form of these Terms of Use and the lack of signing by the parties hereto
         to execute these Terms of Use.
       </p>
-      <h2>11. CONTACT US</h2>
+      <h2>12. CONTACT US</h2>
       <p>
         In order to resolve a complaint regarding the Site or to recieve further
         information regarding use of the Site, please contact us at:
