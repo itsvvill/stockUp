f3b3e4fbde314b3804891414858a7cfebd2a0e2c
itsvvill <w.lewis.eason@gmail.com>
2023-03-12 08:20:24 -0700

    removed unneeded import from useDeleteUser hook

--- src/hooks/useDeleteUser.js
+++ src/hooks/useDeleteUser.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-// import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useDeleteUser = () => {
