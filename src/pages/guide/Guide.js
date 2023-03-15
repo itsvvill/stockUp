4afdf1dd70244b51d5deeb4371be911fda0cc357
itsvvill <w.lewis.eason@gmail.com>
2023-03-15 08:54:10 -0700

    added framer-motion effects to guide page

--- src/pages/guide/Guide.js
+++ src/pages/guide/Guide.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 //styles and icons
 import styles from './Guide.module.css';
@@ -23,9 +24,11 @@ export default function Guide() {
           Login to StockUp, try it out using a guest account, or sign up for a
           new account by clicking the button below.
         </p>
-        <Link to="/login" className={styles.button}>
-          Login
-        </Link>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <Link to="/login" className={styles.button}>
+            Login
+          </Link>
+        </motion.div>
       </div>
       <div className={styles.card}>
         <h2 className={styles['card-heading']}>
@@ -35,9 +38,11 @@ export default function Guide() {
           Search for real-time prices of a single stock or create a watchlist to
           track a group of stocks.
         </p>
-        <Link to="/stocks" className={styles.button}>
-          Stocks
-        </Link>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <Link to="/stocks" className={styles.button}>
+            Stocks
+          </Link>
+        </motion.div>
       </div>
       <div className={styles.card}>
         <h2 className={styles['card-heading']}>
@@ -47,9 +52,11 @@ export default function Guide() {
           Use our transaction list feature to easily categorize and sort your
           finances.
         </p>
-        <Link to="/transactions" className={styles.button}>
-          Transactions
-        </Link>
+        <motion.div whileHover={{ scale: 1.1 }}>
+          <Link to="/transactions" className={styles.button}>
+            Transactions
+          </Link>
+        </motion.div>
       </div>
       {/* Home page animation */}
       <ul className={styles['logos-container']}>
