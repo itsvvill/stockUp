5ae7515491c9ae7bda8528aec20b6bad8e2a5f49
itsvvill <w.lewis.eason@gmail.com>
2023-04-08 10:51:18 -0700

    added final section to TOS

--- src/pages/privacy/TermsOfService.js
+++ src/pages/privacy/TermsOfService.js
@@ -735,7 +735,15 @@ export default function TermsOfService() {
         retention of non-electronic records, or to payments or the granting of
         credits by any means other than electronic means.
       </p>
-      <h2>24. CONTACT US</h2>
+      <h2>24. CALIFORNIA USERS AND RESIDENTS</h2>
+      <p>
+        If any complaint with us is not satisfactorily resolved, you can contact
+        the Complaint Assistance Unit of the Division of Consumer Services of
+        the California Department of Consumer Affairs in writing at 1625 North
+        Market Blvd., Suite N 112, Sacramento, California or by telephone at
+        (800) 952-5210 or (916) 445-1254.
+      </p>
+      <h2>25. CONTACT US</h2>
       <p>
         In order to resolve a complaint regarding the Site or to recieve further
         information regarding use of the Site, please contact us at:
